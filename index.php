8df6280d082947bff772d6790f9b6b5332658877
Rodger Yonley <rodgeryonley@Rodgers-MacBook-Pro.local>
2018-05-27 00:22:24 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rodgeryonley
+ * Date: 5/21/18
+ * Time: 12:01 AM
+ */
+
+phpinfo();
+
+$myarr = array('a','b','c');
+$current_item = null;
+foreach($myarr as $item)
+{
+    $current_item = $item;
+}
